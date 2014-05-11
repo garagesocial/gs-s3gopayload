d4071af69b52cb50c99a6524c1d2f7768bb614b1
mrassi <maxime@garagesocial.com>
2014-05-10 20:08:23 -0400

    Updated README, Reformatted code and variable

--- README.md
+++ README.md
@@ -1,36 +1,61 @@
-S3GoPayload
+gs_s3gopayload
 ===============
 
-Intro
--------------------------
+
 This script takes for input an **array of paths** to local **files** and **directories** in your git project. From there the script will clone the structure and files into a zip file (named after the git sha of the HEAD). Finally the script will upload this zip file to S3 for retrieval on the deployed systems and extraction.
 
-Visualize
--------------------------
+## Overview
 ![diagram](https://github.com/garagesocial/gs-s3gopayload/blob/master/doc/diagram.PNG?raw=true)
 
-Advantages
--------------------------
-Keep your git repo fast and clutter free from things such as the 'vendor' directory when using composer. This vendor folder is required to run your application but can contains hundreds of thousands of files written by third parties. This script allows to package it all up and make it readily available online for download and extraction on your live instances.
-
-Dependencies
--------------------------
+## Requirements
 * `easy_install GitPython`
 * `easy_install boto`
 
-Usage
--------------------------
+## Advantages
+Keep your git repo fast and clutter free from things such as the 'vendor' directory when using composer. This vendor folder is required to run your application but can contains hundreds of thousands of files written by third parties. This script allows to package it all up and make it readily available online for download and extraction on your live instances.
+
+## Requirements
+
+  * GitPython Driver
+    * `easy_install GitPython`
+    * `easy_install boto`
+
+## Usage
+* [Instantiate and Run](#instantiate-run)
+* [Server Download](#server-download)
+* [Server Download Specific](#server-download-specific)
+
+
+<a name="instantiate-run"></a>
+## Instantiate and Run
+
 ```python
 import s3gopayload
 s3gp = s3gopayload.s3gopayload('bucket name', 'accesskeyid', 'secretkey', '/home/gs/srp3j', ['someProjectFolder/vendo', 'someProjectFolder/config' ]
 s3gp.start()
 ```
 
-Server Download
--------------------------
+gs_s3gopayload is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT)
+
+<a name="server-download"></a>
+## Server Download
 Finally on your beanstalk config set for download and extraction
 ```python
 container_commands:
    "00-fetch-flash-clone":
       command: "cd /var/app/ondeck; wget https://s3.amazonaws.com/my-s3/latest.zip; unzip latest.zip; rm latest.zip"
 ```
+
+
+<a name="server-download-specific"></a>
+## Server Download Specific
+To make use of the rollback feature in beanstalk, we need to keep a specific copy of the specific file. Since latest.zip gets overwritten after every push we can't rely on this. This below method allows for the precise retrieval of the correct zip and allows for rollbacks.
+```python
+container_commands:
+   "00-fetch-flash-clone":
+      command: "cd /var/app/ondeck; export git_sha=$(cat /var/log/eb-version-deployment.log | grep -oP 'git-\\K[A-Fa-f\\d]+' | tail -1); wget https://s3.amazonaws.com/my-s3/master/$git_sha.zip; unzip -o $git_sha.zip; rm $git_sha.zip"
+```
+
+### License
+gs_s3gopayload is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT)
+
--- s3gopayload.py
+++ s3gopayload.py
@@ -1,143 +1,172 @@
 #!/usr/bin/python
-# Filename: s3gopayload.py
-#
-# The MIT License (MIT)
-#
-# Copyright (c) 2013 Garagesocial, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
-# Genesys - 2013/12/04 - Maxime Rassi <maxime@garagesocial.com>
+###############################################################################
+### Garagesocial, Inc. - https://www.garagesocial.com
+###############################################################################
+###
+### Filename:  gs_s3gopayload.py
+###
+### About:     Python script to deploy a set of files and directories as a hash
+###            named zip to Amazon S3. Useful for integration with Beanstalk
+###            type systems allowing to keep the git repo free of non essential
+###            clutter such as the vendor directory with composer yet provide a
+###            venue for re-integration.
+###
+### Usage:
+###            s3gp = gs_s3gopayload(
+###                     'bucket name', 'accesskeyid',
+###                     'secretkey', '/home/gs/srp3j',
+###                     ['someProjectFolder/vendo', 'someProjectFolder/config']
+###
+###            s3gp.start()
+###
+### Dependencies:
+###            easy_install GitPython
+###            easy_install boto
+###
+### Documentation Reference: https://docs.python.org/devguide/documenting.html
+###
+###############################################################################
+###
+### The MIT License (MIT)
+###
+### Copyright (c) 2014 Garagesocial, Inc.
+###
+### Permission is hereby granted, free of charge, to any person obtaining a
+### copy of this software and associated documentation files (the "Software"),
+### to deal in the Software without restriction, including without limitation
+### the rights to use, copy, modify, merge, publish, distribute, sublicense,
+### and/or sell copies of the Software, and to permit persons to whom the
+### Software is furnished to do so, subject to the following conditions:
+###
+### The above copyright notice and this permission notice shall be included in
+### all copies or substantial portions of the Software.
+###
+### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+### IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+### FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+### THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+### LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+### FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+### DEALINGS IN THE SOFTWARE.
+###
+###############################################################################
 
 import os, shutil, zipfile, stat, sys
 from git import *
 from boto.s3.connection import S3Connection
 
-class s3gopayload:
-  # s3 bucket name
-  s3Bucket = ''
-  # s3 access key
-  s3AccessKeyId = ''
-  # s3 secret key
-  s3SecretKey = ''
-  # abs path to git root
-  gitRootAbsPath = ''
-  # relative path (to gitRootAbsPath) to directories and files
-  relPathsToZip = []
-  # name of temp directory to create
-  tmpRootDir = '_s3gopayload'
+class gs_s3gopayload:
+   # s3 bucket name
+   s3_bucket = ''
+   # s3 access key
+   s3_access_id = ''
+   # s3 secret key
+   s3_secret_key = ''
+   # abs path to git root
+   git_root_abs_path = ''
+   # relative path (to git_root_abs_path) to directories and files
+   rel_paths_to_zip = []
+   # name of temp directory to create
+   tmp_root_dir = '_gs_s3gopayload'
 
-  def __init__(self, s3Bucket, s3AccessKeyId, s3SecretKey, gitRootAbsPath, relPathsToZip):
-    self.s3Bucket = s3Bucket
-    self.s3AccessKeyId = s3AccessKeyId
-    self.s3SecretKey = s3SecretKey
-    self.gitRootAbsPath = gitRootAbsPath
-    self.relPathsToZip = relPathsToZip
+   def __init__(self, s3_bucket, s3_access_id, s3_secret_key, git_root_abs_path, rel_paths_to_zip):
+      self.s3_bucket = s3_bucket
+      self.s3_access_id = s3_access_id
+      self.s3_secret_key = s3_secret_key
+      self.git_root_abs_path = git_root_abs_path
+      self.rel_paths_to_zip = rel_paths_to_zip
 
-  def start(self):
-    # instantiate the git object
-    repo = Repo(self.gitRootAbsPath, odbt=GitCmdObjectDB)
-    # get head git commit sha (to be used as the zip file name)
-    headGitSha = str(repo.head.commit.hexsha)
-    # get current branch name
-    headGitBranch = str(repo.head.reference)
-    # combine both values
-    headGitBranchSha = headGitBranch  + '/' + headGitSha
+   def start(self):
+      # instantiate the git object
+      repo = Repo(self.git_root_abs_path, odbt=GitCmdObjectDB)
+      # get head git commit sha (to be used as the zip file name)
+      head_git_sha = str(repo.head.commit.hexsha)
+      # get current branch name
+      head_git_branch = str(repo.head.reference)
+      # combine both values
+      head_git_branch_sha = head_git_branch  + '/' + head_git_sha
 
-    # specify name and location of temp directory to work in (helps for cleanup purposes)
-    tmpDir = os.path.join(self.gitRootAbsPath, self.tmpRootDir, headGitBranchSha)
+      # specify name and location of temp directory to work in (helps for cleanup purposes)
+      tmp_dir = os.path.join(self.git_root_abs_path, self.tmp_root_dir, head_git_branch_sha)
 
-    # if already exists delete it and recreate it
-    if os.path.exists(tmpDir): self.delDirRecursive(tmpDir)
-    # create the temp direc
-    os.makedirs(tmpDir)
+      # if already exists delete it and recreate it
+      if os.path.exists(tmp_dir): self.del_dir_recursive(tmp_dir)
+      # create the temp direc
+      os.makedirs(tmp_dir)
 
-    # destination of output zip file
-    zipOutputPath = tmpDir + '/' + headGitSha + '.zip'
+      # destination of output zip file
+      zip_output_path = tmp_dir + '/' + head_git_sha + '.zip'
 
-    # iterate through paths specified to include in zip
-    for relPath in self.relPathsToZip:
-      absPath = os.path.abspath(relPath)
-      print 'Processing... ' + absPath
-      if os.path.exists(absPath): self.zipDir(self.gitRootAbsPath, absPath, zipOutputPath)
+      # iterate through paths specified to include in zip
+      for relPath in self.rel_paths_to_zip:
+         abs_path = os.path.abspath(relPath)
+         print 'Processing... ' + abs_path
+         if os.path.exists(abs_path): self.zip_dir(self.git_root_abs_path, abs_path, zip_output_path)
 
-    # upload zip to s3
-    self.zipUploadS3(self.s3Bucket, self.s3AccessKeyId, self.s3SecretKey, headGitBranchSha + '.zip', zipOutputPath)
-    self.zipUploadS3(self.s3Bucket, self.s3AccessKeyId, self.s3SecretKey, 'latest.zip', zipOutputPath)
-    # cleanup temp directory
-    self.delDirRecursive(tmpDir)
+      # upload zip to s3
+      self.zip_upload_s3(self.s3_bucket, self.s3_access_id, self.s3_secret_key, head_git_branch_sha + '.zip', zip_output_path)
+      self.zip_upload_s3(self.s3_bucket, self.s3_access_id, self.s3_secret_key, 'latest.zip', zip_output_path)
+      # cleanup temp directory
+      self.del_dir_recursive(tmp_dir)
 
-  # Upload the set of key/filepath to s3
-  #
-  # s3Bucket      (i.e.: mybucket)
-  # s3AccessKeyId (i.e.: H0QGLR8VRK55N07KW35R)
-  # s3SecretKey   (i.e.: K7sFcLas5LohfAURXH1MlJLDoRdSCQq8EdXVQxPN)
-  # s3Key         (i.e.: 41bd49f08f5daad48edd9b2202decg2ac405bfa5.zip)
-  # s3KeySource   (i.e.: myfolder/41bd49f08f5daad48edd9b2202decg2ac405bfa5.zip)
-  def zipUploadS3(self, s3Bucket, s3AccessKeyId, s3SecretKey, s3Key, s3KeySource):
-    connectionInstance = S3Connection(s3AccessKeyId, s3SecretKey)
-    bucketInstance = connectionInstance.get_bucket(s3Bucket)
+   # Upload the set of key/filepath to s3
+   #
+   # @param  string s3_bucket      (ex: mybucket)
+   # @param  string s3_access_id   (ex: H0QGLR8VRK55N07KW35R)
+   # @param  string s3_secret_key  (ex: K7sFcLas5LohfAURXH1MlJLDoRdSCQq8EdXVQxPN)
+   # @param  string s3Key          (ex: 41bd49f08f5daad48edd9b2202decg2ac405bfa5.zip)
+   # @param  string s3KeySource    (ex: myfolder/41bd49f08f5daad48edd9b2202decg2ac405bfa5.zip)
+   # @return void
+   def zip_upload_s3(self, s3_bucket, s3_access_id, s3_secret_key, s3Key, s3KeySource):
+      connection_instance = S3Connection(s3_access_id, s3_secret_key)
+      bucket_instance = connection_instance.get_bucket(s3_bucket)
 
-    # if source file does not exist return
-    if not os.path.isfile(s3KeySource) : return
-    # if key exists delete it
-    if bucketInstance.get_key(s3Key) : bucketInstance.delete_key(s3Key)
+      # if source file does not exist return
+      if not os.path.isfile(s3KeySource) : return
+      # if key exists delete it
+      if bucket_instance.get_key(s3Key) : bucket_instance.delete_key(s3Key)
 
-    # upload file using sha as name
-    if bucketInstance.get_key(s3Key) : bucketInstance.delete_key(s3Key)
-    s3KeyNew = bucketInstance.new_key(s3Key)
-    s3KeyNew.set_contents_from_filename(s3KeySource)
-    # s3KeyNew.set_acl('public-read')
+      # upload file using sha as name
+      if bucket_instance.get_key(s3Key) : bucket_instance.delete_key(s3Key)
+      s3_key_new = bucket_instance.new_key(s3Key)
+      s3_key_new.set_contents_from_filename(s3KeySource)
+      # s3_key_new.set_acl('public-read')
 
+   # Zip the specified path into the specified zip. If zip already exists it will append.
+   #
+   # @param    string rel_to_dir      (ex: /home/gs/s3prj) This is essentially the git_root_abs_path
+   # @param    string source_dir      (ex: /home/gs/s3prj/gs) The rel_to_dir will be eaten out of this path
+   # @param    string zip_output_path (ex: /home/gs/s3prj/tmp/41bd49f08f5daad48edd9b2202decg2ac405bfa5/41bd49f08f5daad48edd9b2202decg2ac405bfa5.zip)
+   # @return   void
+   def zip_dir(self, rel_to_dir, source_dir, zip_output_path):
+      # if zip file already exists then append to it otherwise write a new one
+      zip_mode = "a" if os.path.isfile(zip_output_path) else "w"
+      with zipfile.ZipFile(zip_output_path, zip_mode, zipfile.ZIP_DEFLATED) as zip:
+         for root, dirs, files in os.walk(source_dir):
+            # add empty directories
+            zip.write(root, os.path.relpath(root, rel_to_dir))
+            for file in files:
+               filename = os.path.join(root, file)
+               # regular files only
+               if os.path.isfile(filename):
+                  zip_specific_path = os.path.join(os.path.relpath(root, rel_to_dir), file)
+                  zip.write(filename, zip_specific_path)
 
-  # Zip the specified path into the specified zip. If zip already exists it will append.
-  #
-  # relToDir      (i.e.: /home/gs/s3prj) This is essentially the gitRootAbsPath
-  # sourceDir     (i.e.: /home/gs/s3prj/gs) The relToDir will be eaten out of this path
-  # zipOutputPath (i.e.: /home/gs/s3prj/tmp/41bd49f08f5daad48edd9b2202decg2ac405bfa5/41bd49f08f5daad48edd9b2202decg2ac405bfa5.zip)
-  def zipDir(self, relToDir, sourceDir, zipOutputPath):
-    # if zip file already exists then append to it otherwise write a new one
-    zipMode = "a" if os.path.isfile(zipOutputPath) else "w"
-    with zipfile.ZipFile(zipOutputPath, zipMode, zipfile.ZIP_DEFLATED) as zip:
-      for root, dirs, files in os.walk(sourceDir):
-        # add empty directories
-        zip.write(root, os.path.relpath(root, relToDir))
-        for file in files:
-          filename = os.path.join(root, file)
-          # regular files only
-          if os.path.isfile(filename):
-            zipSpecificPath = os.path.join(os.path.relpath(root, relToDir), file)
-            zip.write(filename, zipSpecificPath)
+   # Recursively delete the target_dir and handle the permission problems that could arise in windows
+   # Credits: Epcylon @ SO
+   #
+   # @param    string  target_dir   (ex: /home/gs/s3prj/tmp/41bd49f08f5daad48edd9b2202decg2ac405bfa5)
+   # @return   void
+   def del_dir_recursive(self, target_dir):
+      for root, dirs, files in os.walk(target_dir, topdown=False):
+         for name in files:
+            filename = os.path.join(root, name)
+            os.chmod(filename, stat.S_IWUSR)
+            os.remove(filename)
+         for name in dirs:
+            os.rmdir(os.path.join(root, name))
+      shutil.rmtree(target_dir)
 
-  # Recursively delete the targetDir and handle the permission problems that could arise in windows
-  # Credits: Epcylon @ SO
-  #
-  # targetDir (i.e.: /home/gs/s3prj/tmp/41bd49f08f5daad48edd9b2202decg2ac405bfa5)
-  def delDirRecursive(self, targetDir):
-    for root, dirs, files in os.walk(targetDir, topdown=False):
-      for name in files:
-        filename = os.path.join(root, name)
-        os.chmod(filename, stat.S_IWUSR)
-        os.remove(filename)
-      for name in dirs:
-        os.rmdir(os.path.join(root, name))
-    shutil.rmtree(targetDir)
-
-# End of s3gopayload.py
+# End of gs_s3gopayload.py
 
